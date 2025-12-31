b51124cd1cb0a86ff6a7d5ec5b9e1141fd346f6e
akashseth-ifp <akash.seth@infocusp.com>
2025-12-31 16:53:03 +0530

    added origin to cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,10 @@ const app: Express = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
